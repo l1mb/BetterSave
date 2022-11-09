9006ae5eae549c3df2292dda6c237c24da077073
Jean Korzun <sergeev.s1012@gmail.com>
2022-11-09 15:11:08 +0300

    Add entities

---
+++ Backend/AuthService/DAL/Entities/Spending.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthServiceApp.DAL.Entities
+{
+    public class Spending: BaseEntity
+    {
+        public string Name { get; set; }
+        public string Cost { get; set; }
+        public DateTime SpendingDate { get; set; }
+        public SpendingCategory Category { get; set; }
+
+    }
+}
---
+++ Backend/AuthService/DAL/Entities/SpendingCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthServiceApp.DAL.Entities
+{
+    public class SpendingCategory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
