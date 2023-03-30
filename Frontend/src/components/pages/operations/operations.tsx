45108dadac8869f791eda2b87e3e2dceb3122ac3
Siarhei Siarheyeu <sergeev.s1012@gmail.com>
2023-03-30 23:55:07 +0300

    Add datepicker

--- Frontend/src/components/pages/operations/operations.tsx
+++ Frontend/src/components/pages/operations/operations.tsx
@@ -1,5 +1,6 @@
 import { BaseProps } from "@/types/props/defaultProps";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import DatePicker from "./datePicker/datePicker";
 import OperaionType, { OperaionTypeProps } from "./operationType/operationType";
 
 type OperationProps = BaseProps;
@@ -7,6 +8,27 @@ type OperationType = "income" | "outcome" | "balance";
 
 function Operations({}: OperationProps) {
   const [operationType, setOperationType] = useState<OperationType>("outcome");
+
+  // dates
+  const [date, setDate] = useState(new Date());
+  const handleIncrement = () => {
+    setDate((prevState) => {
+      const t = prevState;
+      t.setMonth(t.getMonth() + 1);
+      return new Date(t);
+    });
+  };
+  const handleDecrement = () => {
+    setDate((prevState) => {
+      const t = prevState;
+      t.setMonth(t.getMonth() - 1);
+      return new Date(t);
+    });
+  };
+
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
 
   const headerLine: OperaionTypeProps[] = [
     {
@@ -23,15 +45,23 @@ function Operations({}: OperationProps) {
     },
   ];
   return (
-    <div className="mx-auto my-24 w-[566px]">
+    <div className="mx-auto my-24 flex w-[566px] flex-col">
       {/* Header line */}
-      <div className="w-fu flex justify-evenly gap-4">
+      <div className=" flex justify-evenly gap-4">
         {headerLine.map((x, index) => (
           <>
             <OperaionType label={x.label} onSelect={x.onSelect} />
             {index !== headerLine.length - 1 && <span className="w-[1px] rounded bg-gray-700" />}
           </>
         ))}
+      </div>
+      <div className="my-16 flex justify-center">
+        <DatePicker
+          start={date}
+          end={new Date(new Date(date).getTime() + 30 * 24 * 60 * 60 * 1000)}
+          handleIncrement={handleIncrement}
+          handleDecrement={handleDecrement}
+        />
       </div>
     </div>
   );
