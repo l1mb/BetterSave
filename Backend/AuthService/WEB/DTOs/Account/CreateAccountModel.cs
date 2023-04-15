eaafbeed07a1f61009c3e5c69c528f63556c8f38
Siarhei Siarheyeu <sergeev.s1012@gmail.com>
2023-04-15 14:19:46 +0300

    Inject repos

--- Backend/AuthService/WEB/DTOs/Account/CreateAccountModel.cs
+++ Backend/AuthService/WEB/DTOs/Account/CreateAccountModel.cs
@@ -2,10 +2,17 @@
 {
     public class CreateAccountModel
     {
+        /// <example>Prior</example>
         public string Name { get; set; }
+        /// <example>cyan</example>
         public string IconColor { get; set; }
+        /// <example>attach_money</example>
         public string IconName { get; set; }
+        /// <example>0</example>
         public float Balance { get; set; }
+        /// <summary>
+        /// User id
+        /// </summary>
         public Guid UserId { get; set; }
     }
 }
